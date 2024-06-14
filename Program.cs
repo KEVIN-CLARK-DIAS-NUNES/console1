7fb5db77c2176af3889dbb0d87e0dd7d322b3bbb
Admin <Admin@PC-4>
2024-06-13 21:22:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+using System.Xml;
+
+namespace console1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Usuario usuario = new Usuario();
+            usuario.nome = "Clark";
+            usuario.login = "root";
+            usuario.senha = "qwe123";
+
+            string login;
+            string senha;
+
+            do {
+                Console.WriteLine("Digite seu login");
+                login = Console.ReadLine();
+
+                Console.WriteLine("Digite sua senha");
+                senha = Console.ReadLine();
+
+            } while(!usuario.Logar(login, senha));
+
+        }
+    }
+}
